d188dddf04b057c71ffbf64edc27ec697945259e
John Wang <jzw2@illinois.edu>
2019-03-27 13:36:14 -0500

    Moved to codegen file

--- src/test.cpp
+++ src/test.cpp
@@ -5,16 +5,27 @@
 #include <iostream>
 
 //extern std::unique_ptr<llvm::Module> naruto::sModule;
+void intTest() {
+  naruto::ASTInt a;
+  llvm::Value * 
+
+}
+
 int main(void)
 {
 	std::string input = "finonacci : ( nth - 1 ) no jutsu + fibonacci : nth - 2 no jutsu";
 	std::vector<naruto::Lex> lexes;
   naruto::sModule = std::make_unique<llvm::Module>("sugma", naruto::sContext);
 	naruto::naruto_lexize(input, lexes);
+
+
+  
 	for(auto item : lexes)
 	{
 		//printLex(item);
 	}
+
+
 	naruto::ASTExpr expression;
 	expression.parse(lexes, 0);
 	expression.print();
